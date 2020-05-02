802c598652fa09b6c6c4473c14fc0253d77d0035
aysola <aysola@umich.edu>
2020-05-02 16:18:37 -0700

    updated setup.sh

--- setup.sh
+++ setup.sh
@@ -3,3 +3,6 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
 sudo apt-get install docker-ce
+sudo curl -L https://github.com/docker/compose/releases/download/1.23.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo usermod -aG docker ${USER}
